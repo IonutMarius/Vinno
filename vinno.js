7c3c3c8934f630e5cad7f8c223d3c96597735129
Vlad Iulian Alexandru <v.iulian.alexandru@gmail.com>
2015-01-17 15:47:28 +0200

    Added the javascript file instead of inline script

---
+++ vinno.js
@@ -0,0 +1,6 @@
+	function pressButton (tab) {
+  // But for now, let's just make sure what we have so
+  // far is working as expected.
+  alert('The browser action was clicked! Yay!');
+}
+chrome.browserAction.onClicked.addListener(pressButton);
