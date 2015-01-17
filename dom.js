0d2fbe97aca89aea98a88260a14c7d92c01f35d0
Ionut <barsanionut62@yahoo.com>
2015-01-17 17:42:04 +0200

    updated request handling

--- dom.js
+++ dom.js
@@ -1,6 +1,17 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
- if (request.action === "getDOM")
-   sendResponse({dom: "The dom that you want to get"});
- else
-   sendResponse({}); // Send nothing..
+    switch (request.action){
+        case "getDOM":
+            sendResponse({dom: "The dom that you want to get"});
+            break;
+        case "getVideoId":
+            var videoId;
+            chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+                var activeTab = arrayOfTabs[0];
+                var match = activeTab.url.match(/[?&]v=([^&]+)/);
+                videoId = match[1];
+            });
+            
+            sendResponse({id: videoId});
+            break;
+    }
 });
