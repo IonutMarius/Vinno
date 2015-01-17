0f1360cd467ca9706493f12b8cd82c9336c34187
Ionut <barsanionut62@yahoo.com>
2015-01-17 16:28:16 +0200

    removed vinno.js from content script

--- vinno.js
+++ vinno.js
@@ -1,7 +1,22 @@
+function pressButton (tab) {
 
-	function pressButton (tab) {
+    function pressButton (tab) {
+        // But for now, let's just make sure what we have so
+        // far is working as expected.
+        alert('The browser action was clicked! Yay!');
+    }
+}
+chrome.browserAction.onClicked.addListener(pressButton);
 
- 	 alert($("#eow-title").attr("title"));
-	}
-	chrome.browserAction.onClicked.addListener(pressButton);
+function getVideoID(){
+    var videId;
+    chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+        var activeTab = arrayOfTabs[0];
+        var match = activeTab.url.match(/[?&]v=([^&]+)/);
+        videoId = match[1];
+    });
 
+    return videId;
+}
+
+alert(getVideoID());
