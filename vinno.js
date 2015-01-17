ff6c2c97fd0c413fb9be5f0ebb59af06b25425af
Ionut <barsanionut62@yahoo.com>
2015-01-17 17:34:11 +0200

    corrected getVideoId();

--- vinno.js
+++ vinno.js
@@ -1,11 +1,17 @@
-function getVideoID(){
-    var videId;
+function getVideoID() {
+    var videoId;
     chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
         var activeTab = arrayOfTabs[0];
         var match = activeTab.url.match(/[?&]v=([^&]+)/);
         videoId = match[1];
     });
 
-    alert(videoId);
+    return videoId;
 };
-getVideoID();
+
+$(document).ready(function() {
+    $("#testButton").on("click", function(){
+         console.log(getVideoID());
+    });
+
+});
