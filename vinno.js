d51ce39e5106ba6dfa997c3b36298f5c65f94ffc
Vlad Iulian Alexandru <v.iulian.alexandru@gmail.com>
2015-01-17 16:48:36 +0200

    add default popup to manifest and changed html

--- vinno.js
+++ vinno.js
@@ -2,7 +2,7 @@
 function pressButton (tab) {
     alert('The browser action was clicked! Yay!');
 }
-chrome.browserAction.onClicked.addListener(pressButton);
+chrome.browserAction.onClicked.addListener(getVideoID);
 
 function getVideoID(){
     var videId;
