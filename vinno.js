b0a84ef9bfa953f93746832d100ceefdd156f560
Vlad Iulian Alexandru <v.iulian.alexandru@gmail.com>
2015-01-17 16:38:01 +0200

    pressButton function rework

--- vinno.js
+++ vinno.js
@@ -1,10 +1,6 @@
-function pressButton (tab) {
 
-    function pressButton (tab) {
-        // But for now, let's just make sure what we have so
-        // far is working as expected.
-        alert('The browser action was clicked! Yay!');
-    }
+function pressButton (tab) {
+    alert('The browser action was clicked! Yay!');
 }
 chrome.browserAction.onClicked.addListener(pressButton);
 
