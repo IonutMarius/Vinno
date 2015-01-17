edbea36f649da830e8494383dbebe2250f6ac38d
Vlad Iulian Alexandru <v.iulian.alexandru@gmail.com>
2015-01-17 16:09:01 +0200

    Vinno only one function

--- vinno.js
+++ vinno.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
+
 	function pressButton (tab) {
 
  	 alert($("#eow-title").attr("title"));
 	}
 	chrome.browserAction.onClicked.addListener(pressButton);
-=======
-function pressButton (tab) {
 
-    function pressButton (tab) {
-        // But for now, let's just make sure what we have so
-        // far is working as expected.
-        alert('The browser action was clicked! Yay!');
-    }
-}
-chrome.browserAction.onClicked.addListener(pressButton);
->>>>>>> origin/master
