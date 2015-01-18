80e43353244e10255aa72bc41f0bfa821c59f60c
Ionut <barsanionut62@yahoo.com>
2015-01-18 14:12:36 +0200

    minor changes to dom.js and vinno.js

--- dom.js
+++ dom.js
@@ -6,6 +6,8 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
             break;
         case "getVideoId":
             var videoId;
+            console.log("xxx");
+            console.log(chrome.tabs);
             chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
                 var activeTab = arrayOfTabs[0];
                 var match = activeTab.url.match(/[?&]v=([^&]+)/);
--- vinno.js
+++ vinno.js
@@ -10,10 +10,8 @@ function getTitle () {
 
 function getVideoID(){
     var videoId;
-    chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-        var activeTab = arrayOfTabs[0];
-        var match = activeTab.url.match(/[?&]v=([^&]+)/);
-        videoId = match[1];
+    chrome.tabs.getCurrent(function(tab){
+        console.log(tab.url);
     });
 
     return videoId;
@@ -21,7 +19,7 @@ function getVideoID(){
 
 $(document).ready(function() {
     $("#testButton").on("click", function(){
-         console.log(getVideoID());
+        console.log(getVideoID());
     });
 
 });
