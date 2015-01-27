b653f4a304f737d1875dcee3e217d64b13c9e51a
Vlad Iulian Alexandru <v.iulian.alexandru@gmail.com>
2015-01-27 22:53:26 +0200

    added thumbnail in edit page

--- Chrome-extension/js/vinno.js
+++ Chrome-extension/js/vinno.js
@@ -58,7 +58,7 @@ function loadVideos(){
             if(response.data != undefined){
                 var html = "<h5>My videos</h5>";
                 for(var i = 0;i<response.data.length;i++){
-                    html += "<div class='video' data-id='"+response.data[i].id+"'><div class='details clearfix'><img class='thumbnail' src='"+response.data[i].thumbnailUrl+"'><p class='video-title'>"+response.data[i].title+"</p><div class=\"button-container clearfix\"><button class='btn btn-info btn-xs editBtn'><span class='glyphicon glyphicon-pencil'></span></button><button class='btn btn-danger btn-xs deleteBtn'><span class='glyphicon glyphicon-remove'></span></button></div></div></div></div>";
+                    html += "<div class='video' data-id='"+response.data[i].id+"' data-thumbnail='"+response.data[i].thumbnailUrl+"' data-title='"+response.data[i].title+"'><div class='details clearfix'><img class='thumbnail' src='"+response.data[i].thumbnailUrl+"'><p class='video-title'>"+response.data[i].title+"</p><div class=\"button-container clearfix\"><button class='btn btn-info btn-xs editBtn'><span class='glyphicon glyphicon-pencil'></span></button><button class='btn btn-danger btn-xs deleteBtn'><span class='glyphicon glyphicon-remove'></span></button></div></div></div></div>";
                 }
                 $("#loadedVideos").html(html);
                 $(".deleteBtn").on("click",function(){
@@ -67,7 +67,12 @@ function loadVideos(){
                 });
                 $(".editBtn").on("click",function(){
                     var videoId = $(this).closest(".video").data("id");
-                    editVideo(videoId);
+                    var thumbnailUrl = $(this).closest(".video").data("thumbnail");
+                    var title = $(this).closest(".video").data("title");
+                    var videoId = $(this).closest(".video").data("id");
+                    var username = window.username;
+                    addValuesForEditInSS(thumbnailUrl,title,username,videoId);
+                    window.location.replace("../pages/edit.html");
                 });
             }
         }
@@ -105,8 +110,18 @@ function deleteVideo(videoId){
         }
     });
 }
-function editVideo(videoId){
-     window.location.replace("../pages/edit.html");
+function addValuesForEditInSS(thumbnailUrl,title,username,videoId){
+    sessionStorage["thumbnailUrl"] = thumbnailUrl;
+    sessionStorage["title"] = title;
+    sessionStorage["username"] = username;
+    sessionStorage["userId"] = window.userId;
+    sessionStorage["videoId"] = videoId;
+
+}
+function modifyEditValues(){
+    $(".editThumbnail").attr("src",sessionStorage["thumbnailUrl"]);
+    $(".editTitle").html(sessionStorage["title"]);
+    $(".editUsername").html(sessionStorage["username"]);
 }
 $(document).ready(function() {
     getUserId();
@@ -115,4 +130,7 @@ $(document).ready(function() {
     $("#logoutBtn").on("click", function(){
         logout();
     });
+    if(sessionStorage["thumbnailUrl"]!=undefined){
+        modifyEditValues();
+    }
 });
